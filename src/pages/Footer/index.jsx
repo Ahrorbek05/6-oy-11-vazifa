516dd5fb0860d30070b335d7accaf188249762ff
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-08-05 13:33:14 +0500

    images

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -7,27 +7,27 @@ function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerItems}>
         <div className={styles.footerItem}>
-            <img src="./src/assets/dairy.png" alt="" />
+            <img src="./public/assets/dairy.png" alt="" />
           <h3>Dairy Products</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/fruits.png" alt="" />
+            <img src="./public/assets/fruits.png" alt="" />
           <h3>Vegetables & Fruits</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/condiments.png" alt="" />
+            <img src="./public/assets/condiments.png" alt="" />
           <h3>Spices & Seasonings</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/babyfood.png" alt="" />
+            <img src="./public/assets/babyfood.png" alt="" />
           <h3>Honey</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/grain.png" alt="" />
+            <img src="./public/assets/grain.png" alt="" />
           <h3>Flour</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
